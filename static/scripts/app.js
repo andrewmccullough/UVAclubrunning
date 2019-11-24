6d22264560087a451570232f42ff0244e32705ac
Andrew McCullough <andrew.s.mccullough@gmail.com>
2019-11-24 16:53:39 -0500

    Anthem opens in new tab

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -7,7 +7,7 @@ $(document).on("click", ".contains-expandable > h4" , function(){
 	$(this).parents(".contains-expandable").toggleClass("collapsed");
 });
 
-let anthemLink = "<a href='https://soundcloud.com/user-870446752/sets/club-anthems' id='anthem-link'>now with anthems!</a>";
+let anthemLink = "<a href='https://soundcloud.com/user-870446752/sets/club-anthems' id='anthem-link' target='_blank'>now with anthems!</a>";
 $("#header > .site-title > .container").append(anthemLink);
 
 console.log("Andrew McCullough is a god.");
